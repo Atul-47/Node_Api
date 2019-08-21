759fdb088895e4154e47971d7f343a9748b85ca3
Atul <atulsathyan24@gmail.com>
2019-08-21 12:57:45 +0530

    orders

---
+++ api/routes/orders.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next) => {
+    res.status(200).json({
+        message: 'Orders Get request'
+    });
+});
+
+router.post('/',(req,res,next) => {
+    res.status(201).json({
+        message: 'Orders Post request'
+    });
+});
+
+router.get('/:orderId',(req,res,next) => {
+        res.status(200).json({
+            message:'Special ID',
+            orderId=req.params.orderId
+        });
+});
+
+router.delete('/:orderId',(req,res,next) => {
+    res.status(200).json({
+        message:'Deleted Order!'
+    });
+});
+
+module.exports = router;
